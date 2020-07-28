ad27c47f4d4254bff302910b6a149dc2dc6628d3
mestre <carlos.mestre@gamersclub.com.br>
2020-07-28 00:01:48 -0300

    create project without unecessary files

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <ul>
+        <li>Hot Reload</li>
+        <li>JSX - [J]ava[S]cript [X]ML</li>
+      </ul>
+      
+    </div>
+  );
+}
+
+export default App;
