8cd456c0fb00199594edeed47a9e6edac9d183fe
mestre <carlos.mestre@gamersclub.com.br>
2020-07-28 01:47:53 -0300

    add more ui components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,39 @@
 import React from 'react';
-import Menu from './components/Menu'
+import Menu from './components/Menu';
+import BannerMain from './components/BannerMain';
+import Carousel from './components/Carousel';
+import Footer from './components/Footer';
+import data from './data/initial_data.json';
 
 function App() {
   return (
-    <div>
+    <div style={{ background: "#141414" }}>
       <Menu />
+
+      <BannerMain
+        videoTitle={data.categorias[0].videos[0].titulo}
+        url={data.categorias[0].videos[0].url}
+        videoDescription={"Software Engineering"}
+      />
+
+      <Carousel
+        ignoreFirstVideo
+        category={data.categorias[0]}
+      />
+
+      <Carousel
+        category={data.categorias[1]}
+      />
+
+      <Carousel
+        category={data.categorias[2]}
+      />
+
+      <Carousel
+        category={data.categorias[3]}
+      />
+
+      <Footer></Footer>
     </div>
   );
 }
---
+++ frontend/src/components/Footer/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { FooterBase } from './styles';
+
+function Footer() {
+  return (
+    <FooterBase>
+      <a href="https://www.vinicius-serpa.com/">
+        <h1>Vinicius Serpa</h1>
+      </a>
+      <p>
+        Coding to be bold
+      </p>
+    </FooterBase>
+  );
+}
+
+export default Footer;
