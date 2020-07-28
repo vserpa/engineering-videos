e712e08b8d5dd4ee0acee20e9abea59cf641ce20
mestre <carlos.mestre@gamersclub.com.br>
2020-07-28 00:13:22 -0300

    create menu component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,10 @@
 import React from 'react';
+import Menu from './components/Menu'
 
 function App() {
   return (
     <div>
-      <h1>Hello World</h1>
-      <ul>
-        <li>Hot Reload</li>
-        <li>JSX - [J]ava[S]cript [X]ML</li>
-      </ul>
-      
+      <Menu />
     </div>
   );
 }
---
+++ frontend/src/components/Menu/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Menu() {
+    return (
+        <nav>
+            Enginnering Videos
+        </nav>
+    );
+}
+
+export default Menu;
