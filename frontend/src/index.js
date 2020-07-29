6695ab650f4f92ecabc1e4f446147c314ed3878e
mestre <carlos.mestre@gamersclub.com.br>
2020-07-28 23:24:36 -0300

    create page template

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,12 +4,14 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './index.css';
 import Home from './pages/Home';
 import RegisterVideo from './pages/register/Video';
+import RegisterCategory from './pages/register/Category';
 
 ReactDOM.render(
   <BrowserRouter>
     <Switch>
       <Route path="/" component={Home} exact />
       <Route path="/register/video" component={RegisterVideo} exact />
+      <Route path="/register/category" component={RegisterCategory} exact />
       <Route component={ () => (<div>404</div>)} />
     </Switch>
   </BrowserRouter>,
--- frontend/src/pages/register/Category/index.js
+++ frontend/src/pages/register/Category/index.js
@@ -2,15 +2,15 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import PageDefault from '../../../templates/PageDefault'
 
-function RegisterVideo() {
+function RegisterCategory() {
     return (
         <PageDefault>
-            <h1>Register Video</h1>
-            <Link to="/register/category">
-                Register Category
+            <h1>Register Category</h1>
+            <Link to="/">
+                Home
             </Link>
         </PageDefault>
     )
 }
 
-export default RegisterVideo
+export default RegisterCategory
