0578bc6affcad40e2b2484a4239d4f26d5273b6d
mestre <carlos.mestre@gamersclub.com.br>
2020-07-28 22:35:15 -0300

    install react-router-dom and set first routes

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './index.css';
 import App from './App';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component={App} exact />
+      <Route component={ () => (<div>404</div>)} />
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
