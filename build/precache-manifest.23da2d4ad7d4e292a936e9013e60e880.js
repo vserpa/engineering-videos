e5c470bf9c1321e4802f5e4e934053a015625f3d
mestre <carlos.mestre@gamersclub.com.br>
2020-08-02 13:20:29 -0300

    reorganize directories, package.json on root

---
+++ build/precache-manifest.23da2d4ad7d4e292a936e9013e60e880.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3a6d5f486ae6526cf82839220fa3185",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9c637257994d662d67f9",
+    "url": "/static/css/main.097cfdc5.chunk.css"
+  },
+  {
+    "revision": "0619e7f0ced32f25c2f0",
+    "url": "/static/js/2.bfcdd404.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.bfcdd404.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c637257994d662d67f9",
+    "url": "/static/js/main.95c153f2.chunk.js"
+  },
+  {
+    "revision": "055e94f6602794288cc3",
+    "url": "/static/js/runtime-main.1f1f19a3.js"
+  },
+  {
+    "revision": "df14953bac14e80922ff84420fc422ab",
+    "url": "/static/media/logo_120.df14953b.png"
+  }
+]);
